c8600845acfe7e3a19c5a618873ad71f317796da
Иван Польских <polskikh.ioan@gmail.com>
2026-08-13 14:56:50 +0500

    fix photo reupload

--- frontend/src/app/pages/admin.component.ts
+++ frontend/src/app/pages/admin.component.ts
@@ -359,7 +359,7 @@ interface PriceConflict {
                         @for (photo of existingPhotos(); track photo.id) {
                           <div class="photo-preview-item">
                             <img
-                              [src]="photoBaseUrl + photo.fileName"
+                              [src]="photoBaseUrl + photo.fileName + '?v=' + photo.id"
                               [alt]="photo.fileName"
                             >
                             <button type="button" (click)="deleteExistingPhoto(photo)">
